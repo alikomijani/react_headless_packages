9f89694dd618f7cd76ff97104f48e4775be72e89
Ali Komijani <akpa125@gmail.com>
2022-08-07 23:02:27 +0430

    fix component

---
+++ src/components/LoadingButton.tsx
@@ -0,0 +1,45 @@
+import React, { ElementType, useCallback, useState } from 'react'
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  onClick: (event?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => Promise<any>
+  ProgressComponent?: ElementType
+  Component?: ElementType
+  progressProps: object
+  loadingText: string
+}
+
+const LoadingButton: React.FC<Props> = ({
+  onClick,
+  ProgressComponent = 'progress',
+  Component = 'button',
+  children,
+  progressProps,
+  loadingText = 'loading...',
+  ...props
+}) => {
+  const [loading, setLoading] = useState(false)
+  const _onClick = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      setLoading(true)
+      onClick(e).finally(() => {
+        setLoading(false)
+      })
+    },
+    [onClick],
+  )
+  if (ProgressComponent) {
+    return (
+      <Component {...props} role={'button'} disabled={loading || props.disabled} onClick={_onClick}>
+        {loading ? <ProgressComponent {...progressProps}>{loadingText}</ProgressComponent> : children}
+      </Component>
+    )
+  } else {
+    return (
+      <Component {...props} role={'button'} disabled={loading || props.disabled} onClick={_onClick}>
+        {loading ? loadingText : children}
+      </Component>
+    )
+  }
+}
+
+export default LoadingButton
